46f21ae9e04b3c92711829cfdc7a8be2589be213
kar2inha <karlacombr10@gmail.com>
2025-10-08 10:51:39 -0300

    feat: adicionando o docker.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+   server:{
+   host:"0.0.0.0",
+   watch:{
+    usePolling: true,
+    interval: 100,
+   },
+   port: 5173,
+  },
 })
