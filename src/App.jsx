339cc3f85f68f9598dec8ff9ca50bd65c15eb893
Karla <karlacombr10@gmail.com>
2025-10-08 17:38:10 -0300

    não sei

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,22 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // Preciso atualizar essa tarefa.
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // Não preciso atualizar essa tarefa.
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+    
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -31,7 +47,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,21 @@
+import { ChevronRight } from "lucide-react";
+
 function Tasks(props) {
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+        <li keys={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+          >
+            {task.title}
+          </button>
+
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRight />
+          </button>
+        </li>
       ))}
     </ul>
   );
