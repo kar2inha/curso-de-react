b0e055056596ef109253ef3295db095d26ee1ae8
Karla <karlacombr10@gmail.com>
2025-10-09 00:11:00 -0300

    feat: terminando ??? o AddTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,11 +1,27 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({onAddTaskSubmit}) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return (
-        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-            <input type="text" placeholder="Digite o título da tarefa..."></input>
-            <input type="text" placeholder="Digite a descrição da tarefa..."></input>
-            <button>Adicionar</button>
-        </div>
-    );
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa..."
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa..."
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button onClick={() => onAddTaskSubmit(title, description)} className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+    </div>
+  );
 }
 
 export default AddTask;
